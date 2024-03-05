e7a8733d59a2c2c16a2b19d0f391d00ed97bda89
Haoran Wang <364504829@qq.com>
2024-03-05 08:35:01 +0800

    fix: Python command not found when executing run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+export PATH="/opt/homebrew/anaconda3/bin:${PATH}"
+alias python="/opt/homebrew/anaconda3/bin/python"
 cd ~/Pictures/Wallpapers
 python ./download_wallpaper.py
 export WALLPAPER_NAME=`ls -lt ./Display| head -n 2 | tail -n 1 | awk '{print $9}'`
